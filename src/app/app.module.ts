02f64e25dd7a33dae4c5629efc242922cef289ed
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2023-12-26 20:41:36 -0600

    Add Angular Material and Firebase dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Angular Material
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularmaterialModule } from './angularmaterial/angularmaterial.module';
 import { HomeComponent } from './Components/home/home.component';
-import { Router } from './router/router.module';
 import { NotfoundComponent } from './Components/notfound/notfound.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from './router/router.module';
 
 import { ChatComponent } from './Components/chat/chat.component';
 
 // npm packages
-import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 import { environment } from '../environments/environment.prod';
-import { provideAuth, getAuth } from '@angular/fire/auth';
 
 // HTTP
 import { HttpClientModule } from '@angular/common/http';
 
 // Socket.io
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
-import { provideStorage, getStorage } from '@angular/fire/storage';
 
 const config: SocketIoConfig = {
   url: 'http://localhost:3001', // socket server url;
